7d16ee8ed0785495de6022a224b9f105197d48b4
Chloe Kwon <yoonjeong1104@gmail.com>
2018-07-25 17:42:18 +0900

    initial commit

---
+++ src/main/java/org/dc/examples/samplesceureka/SampleEurekaApplication.java
@@ -0,0 +1,14 @@
+package org.dc.examples.samplesceureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SampleEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleEurekaApplication.class, args);
+	}
+}
---
+++ src/test/java/org/dc/examples/samplesceureka/SampleScEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package org.dc.examples.samplesceureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleScEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
